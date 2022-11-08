96a29298180cda4262b170cfb7ec0285cc9a4f4e
Gabriela Dyworová <gabriela.dyworova@gmail.com>
2022-11-08 13:01:53 +0100

    Přidejte soubory projektu.

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Ahoj jsem virtuální papoušek Lóra, rád opakuji!");
+Console.WriteLine("Napiš něco");
+string vstup;
+vstup = Console.ReadLine();
+string vystup;
+vystup = vstup + vstup;
+Console.WriteLine(vystup);
+Console.ReadKey();
